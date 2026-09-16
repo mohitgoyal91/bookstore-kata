83ddb9b9db24e547edccf6dca1d82c289264c6eb
valogen <admin@valogen.be>
2026-09-16 11:19:01 +0200

    Adding dev profile and readme

--- src/main/java/be/bnpparibasfortis/bookstore/setup/DataInitializer.java
+++ src/main/java/be/bnpparibasfortis/bookstore/setup/DataInitializer.java
@@ -5,11 +5,13 @@ import be.bnpparibasfortis.bookstore.book.repository.entity.BookEntity;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 import java.math.BigDecimal;
 import java.util.List;
 
 @Configuration
+@Profile("dev")
 public class DataInitializer {
 
     @Bean
